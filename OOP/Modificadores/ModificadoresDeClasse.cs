6407239fbcc6d6bfbe90e5127d863f8f3c118958
TIAGO MARQUES <ads.tiagomarques@gmail.com>
2023-10-27 23:41:59 -0300

    modificadores de classe e de membros

---
+++ OOP/Modificadores/ModificadoresDeClasse.cs
@@ -0,0 +1,13 @@
+﻿
+
+
+
+//Public: Permite que a classe seja acessada por qualquer assembly.
+
+//Sealed: Não permite que a classe seja herdada.
+
+//Partial: Permite que a classe tenha seu escopo divido em dois arquivos. Vemos muito sua utilização ao desenvolver para web, por exemplo.
+
+//Static: Especifica que a classes somente tem membros estáticos. Não pode ser instanciada.
+
+//Abstract: Define molde para classes filhas. Não pode ser instanciada e os métodos são implementados nas classes filhas.
---
+++ OOP/Modificadores/ModificadoresDeMembros.cs
@@ -0,0 +1,19 @@
+﻿
+
+
+
+//Public: Permite que os membros das classes seja acessados por qualquer outro no escopo.
+
+//Private: O membro só poderá ser acessado dentro do escopo da classe.
+
+//Protected: O membro pode ser acessado dentro do escopo da classe e também nas classes filhas.
+
+//Internal: Permite acesso somente por classes do mesmo assembly.
+
+//Static: Permite acesso, sem a necessidade da instância de um objeto.
+
+//Abstract: São métodos de classes Abstract que não possuem implementação.
+
+//Virtual: Permite que os métodos sejam sobrescritos por classes filhas.
+
+//Readonly: Limita acesso a somente leitura aos atributos da classe.
